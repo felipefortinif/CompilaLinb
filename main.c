0c67bc365a97f9d5870044b9e149da90fcec92f6
Felipe Fortini Franco <felipefortinif@gmail.com>
2024-06-06 20:23:09 -0400

    Coloquei um print para testar se o codigo foi escrito certo

--- main.c
+++ main.c
@@ -5,7 +5,6 @@
 
 int main(void){
   FILE *myfp;
-  //funcp func;
 
   if ((myfp = fopen ("teste0.linb", "r")) == NULL) {
     perror ("nao conseguiu abrir arquivo!");
@@ -14,11 +13,15 @@ int main(void){
   
   unsigned char codigo[1000];
   funcp func = CompilaLinB(myfp, codigo);
+
+  printf("\nCodigo:");
+  for (int i = 0; i < 10; i++){
+    printf("\n%02x", codigo[i]);  
+  }
   
-  
-  assert(func != NULL);
-  assert((*func)() == 1);
-  printf("OK");
+  //assert(func != NULL);
+  //assert((*func)() == 1);
+  //printf("OK");
   
   return 0;
 }
