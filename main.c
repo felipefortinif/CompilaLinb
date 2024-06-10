5c0c5fd0799ab20a90201f3979108dbf0e32a3d1
Felipe Fortini Franco <felipefortinif@gmail.com>
2024-06-10 12:01:19 -0400

    Update main.c

--- main.c
+++ main.c
@@ -6,22 +6,24 @@
 int main(void){
   FILE *myfp;
 
-  if ((myfp = fopen ("teste0.linb", "r")) == NULL) {
+  if ((myfp = fopen ("teste4.linb", "r")) == NULL) {
     perror ("nao conseguiu abrir arquivo!");
     exit(1);
   }
   
   unsigned char codigo[1000];
+  
   funcp func = CompilaLinB(myfp, codigo);
 
   printf("\nCodigo:");
-  for (int i = 0; i < 10; i++){
+  for (int i = 0; i < 35; i++){
     printf("\n%02x", codigo[i]);  
   }
+  printf("\n");
   
-  //assert(func != NULL);
-  //assert((*func)() == 1);
-  //printf("OK");
+  assert(func != NULL);
+  assert((*func)(6) == 	91);
+  printf("\nOK\n");
   
   return 0;
 }
